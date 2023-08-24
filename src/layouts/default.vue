fc8fe1148e9f5c0427ee99904c3227da756a704e
Akayy <techakayy@gmail.com>
2023-08-24 13:02:59 +1000

    33 Hydrated FloatingCart button which we missed

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,7 +7,10 @@
     <div
       class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
     >
-      <FloatingCart class="bottom-12 fixed right-2 z-20"></FloatingCart>
+      <FloatingCart
+        class="bottom-12 fixed right-2 z-20"
+        client:load
+      ></FloatingCart>
       <NavBar
         :class="[currentRoute.path === '/' ? 'absolute z-10' : '']"
       ></NavBar>
